e7138d446db75b027825e4a18e7b9703c3e2a9c8
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-04-07 12:21:53 -0300

    moving the useEffect to Landing

--- client/src/routes/Landing.js
+++ client/src/routes/Landing.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LandingContent from '../components/LandingContent';
+import { useDispatch } from 'react-redux';
+import { getAllActivities, getAllCountries } from '../redux/actions';
 
 const Landing = () => {
   console.log(process.env.REACT_APP_API_BASE_URL)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllActivities());
+    dispatch(getAllCountries())
+  }, [])
   return (
     <div>
         <LandingContent />
