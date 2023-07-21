df74f6aa159b2ff552231c9f797d6028cff795ef
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-07-21 11:19:04 -0300

    fixing some issues in details

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div id='footer'>
-        <div id='footer-content'>
-          <p>© Created by Ivan Consorte</p>
-          <p>Mar del Plata, Buenos Aires, Argentina - 2023</p>
-        </div>
+      <div id='footer-content'>
+        <p>© Created by Ivan Consorte</p>
+        <p>Mar del Plata, Buenos Aires, Argentina - 2023</p>
+      </div>
     </div>
   )
 }
